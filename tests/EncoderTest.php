6caba74fcf8473c4cbd29088c88a8e0a9788ef7b
AL <javachina@foxmail.com>
2019-10-28 00:25:29 +0800

    initial commit

--- tests/EncoderTest.php
+++ tests/EncoderTest.php
@@ -6,7 +6,7 @@
 require '../vendor/autoload.php';
 
 $str = 'a test string';
-$str2 = al\helper\Encoder::encode($str);
-$str3 = \al\helper\Encoder::decode($str2);
+$str2 = \AlHepler\Encoder::encode($str);
+$str3 = \AlHepler\Encoder::decode($str2);
 echo $str2 . PHP_EOL;
 echo $str3 . PHP_EOL;
